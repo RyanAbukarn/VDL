4c0bb9b1206592a2cfb1f1c5078399fa2f2407c7
RYAN ABUKARN <61808006+RyanAbukarn@users.noreply.github.com>
2022-07-11 09:01:54 -0400

    style and more features

--- file/urls.py
+++ file/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.form_upload, name='form-upload'),
+    path('upload', views.form_upload, name='form-upload'),
+    path('', views.documents, name='documents'),
+    path('<str:pk>/', views.show, name='show'),
+
+
 ]
--- file/views.py
+++ file/views.py
@@ -1,3 +1,5 @@
+from file.models import Document
+
 from django.shortcuts import redirect, render
 from file.forms import DocumentForm
 
@@ -6,6 +8,9 @@ def form_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
+            new_ = Document(name=request.FILES['file'].name,
+                            description=request.POST['description'])
+            new_.save()
             form.save()
             return redirect('/')
     else:
@@ -13,3 +18,13 @@ def form_upload(request):
     return render(request, 'upload_file.html', {
         'form': form
     })
+
+
+def documents(request):
+    documents = Document.objects.all()
+    return render(request, 'documents.html', {'documents': documents})
+
+
+def show(request, pk):
+    document = Document.objects.get(id=pk)
+    return render(request, 'show.html', {'document': document})
